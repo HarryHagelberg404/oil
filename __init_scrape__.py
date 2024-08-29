432eed03d19b6585edd4b97a5dda423fd26d4930
Harry Hagelberg <harry.hagelberg@forefront.se>
2024-08-29 14:44:22 +0200

    Fixed ansible playbook and configured cron jobs

--- __init_scrape__.py
+++ __init_scrape__.py
@@ -1,13 +1,9 @@
 import program.scrape as scrape
-import program.send as send
 
 
 def main():
     # Scrape the oil price
     scrape.main()
-
-    # Send the report via email
-    send.main()
 
 
 if __name__ == "__main__":
---
+++ __init_send__.py
@@ -0,0 +1,10 @@
+import program.send as send
+
+
+def main():
+    # Send the report via email
+    send.main()
+
+
+if __name__ == "__main__":
+    main()
